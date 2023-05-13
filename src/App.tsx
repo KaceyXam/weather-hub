82a85a0f1bd732026c6b8fb66da10770db663a95
kaceykniffen <kaceykniffen@gmail.com>
2023-05-12 19:12:38 -0500

    Scafold out HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
 function App() {
 	return (
 		<>
-			<div>Hello Weather</div>
+			<form>
+				<input type="text" id="city" />
+				<button type="submit">Fetch Weather</button>
+			</form>
+			<section className="weather-wrapper">
+				<section>
+					<img src="/vite.svg" alt="Weather Icon" />
+					<div>
+						<h2>Today</h2>
+						<h1>City</h1>
+						<p>Temperature</p>
+						<p>Sky Conditions</p>
+					</div>
+				</section>
+				<ul className="next-4-days">
+					<li>
+						<h3>tomorrow</h3>
+						<img src="/vite.svg" alt="Weather Icon" />
+						<p>Temperature</p>
+					</li>
+					<li>
+						<h3>tomorrow</h3>
+						<img src="/vite.svg" alt="Weather Icon" />
+						<p>Temperature</p>
+					</li>
+					<li>
+						<h3>tomorrow</h3>
+						<img src="/vite.svg" alt="Weather Icon" />
+						<p>Temperature</p>
+					</li>
+					<li>
+						<h3>tomorrow</h3>
+						<img src="/vite.svg" alt="Weather Icon" />
+						<p>Temperature</p>
+					</li>
+				</ul>
+			</section>
 		</>
 	);
 }
